582db3daecf09000984f361816c2a1f09fe194fb
Mwixa <bed-com-25-24@unima.ac.mw>
2026-04-01 12:59:42 +0200

    Add files via upload
    introduction to pointers that displays address of variable name

---
+++ introtopointers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int* ppointer =nullptr;
+    int integervar = 5;
+    cout <<"integervar :"<<integervar<<endl;
+    cout <<"address of integervar: "<< &integervar<<endl;
+    cout << "ppointer: "<< ppointer<<endl;
+    return 0;
+}
